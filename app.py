13b1b39f31483cfeebbaf4f02dcf69c4a1d70b0c
milosz98 <milosz.salwa@erptech.pl>
2023-07-05 11:15:40 +0200

    __init__

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import sqlite3
-from datetime import datetime
+
 
 class App:
     def __init__(self, database_name):
-        self.conn = sqlite3.connect(database_name)
-        self.cursor = self.conn.cursor()
+        self.conn = sqlite3.connect(database_name) # Tworzy połączenie do bazy danych o nazwie "database_name". Połączenie przechowywane jest w atrybucie "conn"
+        self.cursor = self.conn.cursor() # Tworzy kursor, który będzie używany do wykonywania poleceń SQL w bazie danych. Kursor przechowywany jest w atrybucie "cursor"
 
         # Tworzenie tabeli
         self.cursor.execute("""
@@ -14,7 +14,8 @@ class App:
                 content TEXT,
                 created_at TEXT
             )
-        """)
-        self.conn.commit()
+        """) # Wykonuje polecenie SQL w bazie danych, tworząc tabelę o nazwie "entries" w przypadku jej nieistnienia. Tabela ta będzie miała kolumny: "id" (typ INTEGER, klucz główny, automatycznie inkrementowany), "title" (typ TEXT), "content" (typ TEXT) i "created_at" (typ TEXT)
+
+        self.conn.commit() # Zatwierdza wszystkie zmiany dokonane w bazie danych. W tym przypadku, zapisuje utworzoną tabelę do bazy danych
 
     
