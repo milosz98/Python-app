e28bb1f7f43d0d70500d3862502fbbf22c5a51a2
milosz98 <milosz.salwa@erptech.pl>
2023-07-05 11:35:09 +0200

    addEntry

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class App:
 
 # -------------------------------------------------------------------------------------------------------------------------------------
     
-    def addEntry(self, title, content):
-        created_at = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.cursor.execute("INSERT INTO entries (title, content, created_at) VALUES (?, ?, ?)", (title, content, created_at))
+    def addEntry(self, title, content): # Metoda dodająca nowy wpis do tabeli "entries" używając wartości przekazanych jako argumenty "title" i "content" oraz bieżącej daty i czasu
+        created_at = datetime.now().strftime("%d-%m-%Y %H:%M:%S") # Zmienna "created_at" przechowuje bieżącą datę i czas
+        self.cursor.execute("INSERT INTO entries (title, content, created_at) VALUES (?, ?, ?)", (title, content, created_at)) # Wykonuje polecenie INSERT INTO w bazie danych. Wstawia nowy wpis (rekord) do tabeli "entries" z wartościami kolumn "title", "content" i "created_at". Wartości tych kolumn są przekazywane jako parametry za pomocą znaków zapytania (?), a ich rzeczywiste wartości są dostarczane jako krotka (title, content, created_at)
         self.conn.commit()
