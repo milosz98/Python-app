05797990dddb01fa6c3fc59cda2ebeea7fefd1cd
milosz98 <milosz.salwa@erptech.pl>
2023-07-05 11:08:28 +0200

    __init__

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 import sqlite3
+from datetime import datetime
 
 class App:
-    def __init__(self):
-        pass
+    def __init__(self, database_name):
+        self.conn = sqlite3.connect(database_name)
+        self.cursor = self.conn.cursor()
 
-    def createTable(): # Metoda służąca do tworzenia tabeli w bazie danych
+        # Tworzenie tabeli
+        self.cursor.execute("""
+            CREATE TABLE IF NOT EXISTS entries (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT,
+                content TEXT,
+                created_at TEXT
+            )
+        """)
+        self.conn.commit()
 
-        conn = sqlite3.connect('wpisy.db') # Stworzenie nowej bazy danych (lub połączenie się z już istniejącą)
-        c = conn.cursor() # Utworzenie kursora umożliwiającego wykonanie poleceń SQL na bazie danych, np. wstawianie danych, pobieranie danych itp.
-
-        # Utworzenie tabeli "wpisy"
-        c.execute("""CREATE TABLE IF NOT EXISTS wpisy
-        (id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        tresc TEXT NOT NULL) """) # Ta linia wykonuje polecenie SQL w celu utworzenia tabeli o nazwie 'wpisy'. Tabela ta będzie miała dwa kolumny: id (typu INTEGER z kluczem głównym) oraz tresc (typu TEXT, nie może być pusta). Jeśli tabela już istnieje, to instrukcja CREATE TABLE IF NOT EXISTS zapewnia, że nie zostanie utworzona ponownie
-        
-        # Zatwierdzenie zmian i zamknięcie połączenia z bazą danych
-        conn.commit() # Ta linia zatwierdza zmiany wprowadzone w bazie danych. W tym przypadku, potwierdza utworzenie tabeli
-        conn.close() # Zamyka połączenie z bazą danych
-
-    # OGÓLNIE: Ten fragment kodu tworzy tabelę 'wpisy' w bazie danych o nazwie 'wpisy.db'. Jeśli tabela już istnieje, to instrukcja CREATE TABLE IF NOT EXISTS zapewnia, że nie zostanie utworzona ponownie.
+    
